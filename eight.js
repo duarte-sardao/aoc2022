a8203277638464a99bf9c777b68ba2483b722901
Dudugs <duartesardao@sapo.pt>
2022-12-08 13:49:19 +0000

    too busy to be doing this but final try after this

--- eight.js
+++ eight.js
@@ -4,6 +4,7 @@ const readline = require('readline');
 var arr = [];
 var linep = 0;
 var last = null;
+var verticals = [[]];
 
 var seeables = 0;
 
@@ -43,6 +44,7 @@ function verifyHeight() {
     for(let y = 0; y <= last; y++) {
       verticalArr.push(arr[y][x].height)
     }
+    verticals.push(verticalArr);
     let maxHeightL = -1;
     let maxHeightR = -1;
     for(let y = 0; y <= last; y++) {
@@ -59,7 +61,34 @@ function verifyHeight() {
 }
 
 function viewing() {
-  
+  let res = [];
+  let prevV = verticals.map(nnn => 0)
+  let prevH = 0; let forH = 0; let forV = 0;
+  for(let y = 1; y < last; y++) {
+    for(let x = 1; x < last; x++) {
+      const val = arr[y][x].height;
+      if(val > arr[y][x-1].height)
+        prevH++;
+      else
+        prevH = 1;
+      if(val > arr[y-1][x].height)
+        prevV[x]++;
+      else
+        prevV[x] = 1;
+      //i think theres a betetr way than repeating this everytime but yolo im too busy
+      forH = arr[y].slice(x+1, last+1).map(nnn => val <= nnn.height).findIndex(element => element)+1;
+      forV = verticals[x].slice(y+1, last+1).map(nnn => val <= nnn).findIndex(element => element)+1;
+      if(forH == 0)
+        forH = (last)-x;
+      if(forV == 0)
+        forV = (last)-y;
+      console.log(val);
+      console.log(String(prevH) + " " +String(prevV[x]) + " " +String(forH) + " " +String(forV))
+      res.push(prevH*prevV[x]*forH*forV);
+    }
+    prevH = 0;
+  }
+  return Math.max.apply(Math, res);
 }
 
 
