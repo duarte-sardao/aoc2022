dd90a8419027cc029756cc8596ccf1bb60cea200
Dudugs <duartesardao@sapo.pt>
2022-12-09 11:48:11 +0000

    not elegant but still got that dog in me

--- nine.hs
+++ nine.hs
@@ -6,11 +6,34 @@ import qualified Data.Set as Set
 getWordsFromFile :: String -> IO [String]
 getWordsFromFile file = readFile file >>= return . words
 
+replace pos newVal list = take pos list ++ newVal : drop (pos+1) list
+
 main :: IO ()
 main = do
   wordList <- getWordsFromFile "input.txt"
   --print((expandMoves wordList []))
-  print((moveRopes (expandMoves wordList []) [] (0,0) (0,0))+1)
+  --print((moveRopes (expandMoves wordList []) [] (0,0) (0,0))+1)
+  print((moveRopes2 (expandMoves wordList []) [] (replicate 10 (0,0))))
+	
+moveRopes2 :: [String] -> [(Int, Int)] -> [(Int, Int)] -> Int
+moveRopes2 [] tailList _ = length (Set.fromList tailList)
+moveRopes2 moves tailList hts = moveRopes2 newmoves newlist newt
+								where (newmoves, newlist, newt) = doMovement2((moves, tailList, hts))
+												  
+doMovement2 :: ([String], [(Int, Int)], [(Int, Int)]) -> ([String], [(Int, Int)], [(Int, Int)])
+doMovement2 ((dir:movelist), tailList, ((hx,hy):lst)) = doMovementT2((movelist, tailList, newlst, 1)) where newlst = [(moveX dir hx hy)] ++ lst
+
+doMovementT2 :: ([String], [(Int, Int)], [(Int, Int)], Int) -> ([String], [(Int, Int)], [(Int, Int)])
+doMovementT2 (movelist, tailList, ys, pos) | pos == 9 = (movelist, tailList ++ [newY], replace pos newY ys)
+										   | otherwise = doMovementT2(movelist, tailList, replace pos newY ys, pos+1)
+										   where newY = updateY (ys !! (pos-1)) (ys !! pos)
+													 
+updateY :: (Int, Int) -> (Int, Int) -> (Int, Int)
+updateY (hx, hy) (tx, ty) | abs diff_x > 1 || abs diff_y > 1 = (tx + signum diff_x, ty + signum diff_y)
+					   | otherwise = (tx, ty)
+					   where (diff_x, diff_y) = (hx-tx, hy-ty)										 
+													 
+												
   
 expandMoves :: [String] -> [String] -> [String]
 expandMoves [] newlist = newlist
