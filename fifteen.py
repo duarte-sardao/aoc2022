99667624acda657851177973da3fa1d2ea96750e
Dudugs <duartesardao@sapo.pt>
2022-12-15 13:08:34 +0000

    using ranges incomp

--- fifteen.py
+++ fifteen.py
@@ -1,23 +1,46 @@
 import re
 
-y = 2000000
-impossible = set()
+lim = 20
+ranges = {}
 
 def thebigapple(xo, yo, xt, yt):
     return abs(xo-xt) + abs(yo-yt)
 
+
+
 def fillout(pos,dist):
-    moved = 0
-    for i in range(0,dist+1):
-        if(pos[1]+i == y or pos[1]-i == y):
-            for j in range(0,dist+1-moved):
-                if(pos[1]+i == y):
-                    impossible.add((pos[0]+j,pos[1]+i))
-                    impossible.add((pos[0]-j,pos[1]+i))
-                if(pos[1]-i == y):
-                    impossible.add((pos[0]+j,pos[1]-i))
-                    impossible.add((pos[0]-j,pos[1]-i))
-        moved += 1
+    for i in range(pos[0]-dist,pos[0]+dist+1):
+        if(i < 0 or i > lim):
+            continue
+        diff = abs(i-pos[0])
+        limback = max(0, pos[1]-dist+diff)
+        limfront = min(lim, pos[1]+dist-diff)
+        if(i == 14):
+            print(str(limback) + " " + str(limfront))
+        try:
+            update = False
+            for j in range(len(ranges[i])):
+                rng = ranges[i][j]
+                if(limback >= rng[0] and limfront <= rng[1]):
+                    update = True
+                    break
+                elif(limback < rng[0] and limfront > rng[1]):
+                    ranges[i][j] = (limback, limfront)
+                    update = True
+                    break
+                elif(limback < rng[0] and limfront <= rng[1] and limfront >= rng[0]):
+                    ranges[i][j] = (limback, rng[1])
+                    update = True
+                    break
+                elif(limback >= rng[0] and limback <= rng[1] and limfront > rng[1]):
+                    ranges[i][j] = (rng[0], limfront)
+                    update = True
+                    break
+            if(not update):
+                ranges[i].append((limback,limfront))
+        except KeyError:
+            ranges[i] = [(limback,limfront)]
+
 
 def find_numbers(string, ints=True):    #cheating?        
     numexp = re.compile(r'[-]?\d[\d,]*[\.]?[\d{2}]*') #optional - in front
@@ -31,20 +54,10 @@ def find_numbers(string, ints=True):    #cheating?
 with open('input.txt', 'r') as f:
     s = list(map(lambda x: find_numbers(x), f.read().strip().split('\n')))
 
-#s = [[8,7,2,10]]
 
 for i in range(len(s)):
     blah = s[i]
     dist = thebigapple(blah[0],blah[1],blah[2],blah[3])
     fillout((blah[0],blah[1]),dist)
 
-for i in range(len(s)):
-    blah = s[i]
-    try:
-        impossible.remove((blah[2], blah[3]))
-    except:
-        continue
-
-
-print(len(impossible))
-
+print(ranges)
